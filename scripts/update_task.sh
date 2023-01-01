1aa221000b6be3e794e67b7add8d1ba5f6d76869
ikenley <ikenley6@gmail.com>
2023-01-01 15:16:01 -0500

    update task def

--- scripts/update_task.sh
+++ scripts/update_task.sh
@@ -11,12 +11,22 @@ echo "OLD_TASK_DEFINITION=$OLD_TASK_DEFINITION"
 # --query 'taskDefinition.revision' \
 # --output text
 
-IMAGE_URI="924586450630.dkr.ecr.us-east-1.amazonaws.com/ik-dev-ecs-standalone-task:0.0.7"
+IMAGE_URI="924586450630.dkr.ecr.us-east-1.amazonaws.com/ik-dev-ecs-standalone-task:554e3df"
 
-NEW_TASK_DEFINITION=$(echo $OLD_TASK_DEFINITION | jq -r '.containerDefinitions[0].image='\"${IMAGE_URI}\")
+NEW_TASK_DEFINITION=$(echo $OLD_TASK_DEFINITION | \
+    jq -r '.containerDefinitions[0].image='\"${IMAGE_URI}\" | \
+    jq 'del(.taskDefinitionArn)' | \
+    jq 'del(.revision)' | \
+    jq 'del(.status)' | \
+    jq 'del(.requiresAttributes)' | \
+    jq 'del(.compatibilities)' | \
+    jq 'del(.registeredAt)' | \
+    jq 'del(.registeredBy)')
 echo "NEW_TASK_DEFINITION=$NEW_TASK_DEFINITION"
 
-#aws ecs register-task-definition --family ik-dev-ecs-standalone-task --cli-input-json "$NEW_TASK_DEFINITION"
+echo "$NEW_TASK_DEFINITION" > new_task_definition.json
+
+aws ecs register-task-definition --family ik-dev-ecs-standalone-task --cli-input-json "$NEW_TASK_DEFINITION"
 
 # TASK_DEFINITION="ik-dev-ecs-standalone-task:${REVISION}"
 # echo "TASK_DEFINITION=$TASK_DEFINITION"
