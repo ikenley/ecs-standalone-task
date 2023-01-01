d833b35efdb2d612655d8dce712be0be16549286
ikenley <ikenley6@gmail.com>
2023-01-01 10:04:52 -0500

    codepipeline scaffolding TODO buildspec

--- terraform/modules/main/cicd.tf
+++ terraform/modules/main/cicd.tf
@@ -2,13 +2,17 @@
 # CI/CD for deployment automation
 #------------------------------------------------------------------------------
 
+locals {
+  codebuild_project_name = "${local.id_prefix}-docker-build-push"
+}
+
 #------------------------------------------------------------------------------
 # CodePipeline
 #------------------------------------------------------------------------------
 
 resource "aws_codepipeline" "this" {
   name     = local.id_prefix
-  role_arn = aws_iam_role.codepipeline_role.arn
+  role_arn = aws_iam_role.codepipeline.arn
 
   artifact_store {
     location = data.aws_ssm_parameter.code_pipeline_s3_bucket_name.value
@@ -28,8 +32,8 @@ resource "aws_codepipeline" "this" {
 
       configuration = {
         "BranchName" : var.git_branch_name
-        "ConnectionArn" : var.codestar_connection_arn
-        "FullRepositoryId" : var.source_full_repository_id
+        "ConnectionArn" : data.aws_ssm_parameter.codestar_connection_arn.value
+        "FullRepositoryId" : "ikenley/ecs-standalone-task"
         "OutputArtifactFormat" : "CODE_ZIP"
       }
     }
@@ -52,176 +56,84 @@ resource "aws_codepipeline" "this" {
       }
     }
   }
-
-  stage {
-    name = "Deploy"
-
-    action {
-      name            = "Deploy"
-      category        = "Deploy"
-      owner           = "AWS"
-      provider        = "ECS"
-      input_artifacts = ["BuildArtifact"]
-      version         = "1"
-
-      configuration = {
-        "ClusterName" : local.ecs_cluster_name
-        "ServiceName" : aws_ecs_service.this.name
-      }
-    }
-  }
-
-  stage {
-    name = "EndToEndTests"
-
-    action {
-      name             = "EndToEndTests"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["SourceArtifact"]
-      output_artifacts = ["E2ETestsArtifact"]
-      version          = "1"
-
-      configuration = {
-        ProjectName = local.e2e_project_name
-      }
-    }
-  }
 }
 
-resource "aws_iam_role" "codepipeline_role" {
-  name = "${var.name}-codepipeline-role"
+resource "aws_iam_role" "codepipeline" {
+  name = "${local.id_prefix}-codepipeline"
 
-  assume_role_policy = <<EOF
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "codepipeline.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_policy" "codepipeline" {
+  name = "${local.id_prefix}-codepipeline"
+  path = "/"
+
+  policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObjectAcl",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${data.aws_ssm_parameter.code_pipeline_s3_bucket_arn.value}",
+        "${data.aws_ssm_parameter.code_pipeline_s3_bucket_arn.value}/*"
+      ]
+    },
+    {
       "Effect": "Allow",
-      "Principal": {
-        "Service": "codepipeline.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+      "Action": [
+        "codestar-connections:UseConnection"
+      ],
+      "Resource": "${data.aws_ssm_parameter.codestar_connection_arn.value}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
     }
   ]
 }
 EOF
 }
 
-resource "aws_iam_role_policy" "codepipeline_policy" {
-  name = "${var.name}-codepipeline-policy"
-  role = aws_iam_role.codepipeline_role.id
-
-  policy = templatefile("${path.module}/codepipeline_policy.tpl", {
-    code_pipeline_s3_bucket_name = var.code_pipeline_s3_bucket_name
-  })
+resource "aws_iam_role_policy_attachment" "codepipeline" {
+  role       = aws_iam_role.codepipeline.name
+  policy_arn = aws_iam_policy.codepipeline.arn
 }
 
 #------------------------------------------------------------------------------
 # CodeBuild
 #------------------------------------------------------------------------------
 
-resource "aws_codebuild_project" "this" {
+resource "aws_codebuild_project" "docker_build" {
   name        = local.codebuild_project_name
-  description = "CodeBuild project for ${var.name}"
+  description = "Docker build and push ${local.id_prefix}"
 
-  service_role = aws_iam_role.codebuild_role.arn
-
-  artifacts {
-    type = "CODEPIPELINE"
-    name = aws_codepipeline.this.name
-  }
-
-  cache {
-    type  = "LOCAL"
-    modes = ["LOCAL_DOCKER_LAYER_CACHE"]
-  }
-
-  environment {
-    type                        = "LINUX_CONTAINER"
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
-    image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = true
-
-    environment_variable {
-      name  = "ENV"
-      value = var.env
-    }
-
-    environment_variable {
-      name  = "BRANCH_NAME"
-      value = var.source_branch_name
-    }
-
-    environment_variable {
-      name  = "AWS_ACCOUNT_ID"
-      value = local.account_id
-    }
-
-    dynamic "environment_variable" {
-      for_each = aws_ecr_repository.this.*
-      content {
-        name  = "${upper(replace(environment_variable.value.name, "${var.name}-", ""))}_IMAGE_REPO_NAME"
-        value = environment_variable.value.name
-      }
-    }
-
-  }
-
-  source {
-    type = "CODEPIPELINE"
-  }
-
-  tags = local.tags
-}
-
-resource "aws_iam_role" "codebuild_role" {
-  name = "${var.name}-codebuild-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "codebuild_policy" {
-  name = "${var.name}-codebuild-policy"
-
-  policy = templatefile("${path.module}/codebuild_policy.tpl", {
-    account_id                   = local.account_id
-    code_pipeline_s3_bucket_name = var.code_pipeline_s3_bucket_name
-    ecr_arns                     = jsonencode(aws_ecr_repository.this.*.arn)
-    codebuild_project_name       = local.codebuild_project_name
-    name                         = var.name
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "codebuild_policy" {
-  role       = aws_iam_role.codebuild_role.name
-  policy_arn = aws_iam_policy.codebuild_policy.arn
-}
-
-#------------------------------------------------------------------------------
-# CodeBuild End-to-end (E2E) test
-#------------------------------------------------------------------------------
-
-resource "aws_codebuild_project" "e2e_test_codebuild" {
-  name        = local.e2e_project_name
-  description = "CodeBuild project for ${var.name} E2E tests"
-
-  service_role = aws_iam_role.e2e_test_codebuild.arn
+  service_role = aws_iam_role.codebuild.arn
 
   artifacts {
     type = "CODEPIPELINE"
@@ -246,23 +158,26 @@ resource "aws_codebuild_project" "e2e_test_codebuild" {
     }
 
     environment_variable {
-      name  = "BRANCH_NAME"
-      value = var.source_branch_name
-    }
-
-    environment_variable {
       name  = "AWS_ACCOUNT_ID"
       value = local.account_id
     }
 
-    dynamic "environment_variable" {
-      for_each = var.e2e_codebuild_env_vars
-      content {
-        name  = environment_variable.key
-        value = environment_variable.value
-      }
+    environment_variable {
+      name  = "DOCKER_USERNAME"
+      type  = "PARAMETER_STORE"
+      value = "/docker/username"
     }
 
+    environment_variable {
+      name  = "DOCKER_PASSWORD"
+      type  = "PARAMETER_STORE"
+      value = "/docker/password"
+    }
+
+    environment_variable {
+      name  = "IMAGE_REPO_URL"
+      value = aws_ecr_repository.this.repository_url
+    }
   }
 
   source {
@@ -272,38 +187,125 @@ resource "aws_codebuild_project" "e2e_test_codebuild" {
   tags = local.tags
 }
 
-resource "aws_iam_role" "e2e_test_codebuild" {
-  name = "${local.e2e_project_name}-codebuild-role"
+resource "aws_iam_role" "codebuild" {
+  name = "${local.id_prefix}-codebuild"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
       },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_policy" "codebuild" {
+  name = "${local.id_prefix}-codebuild"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowECR",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:UploadLayerPart",
+                "ecr:PutImage",
+                "ecr:BatchGetImage",
+                "ecr:CompleteLayerUpload",
+                "ecr:InitiateLayerUpload",
+                "ecr:BatchCheckLayerAvailability"
+            ],
+            "Resource": [
+                "${aws_ecr_repository.this.arn}"
+            ]
+        },
+        {
+            "Sid": "AllowECRAuthorizationToken",
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowS3",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetBucketAcl",
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:GetBucketLocation",
+                "s3:GetObjectVersion"
+            ],
+            "Resource": [
+                "${data.aws_ssm_parameter.code_pipeline_s3_bucket_arn.value}",
+                "${data.aws_ssm_parameter.code_pipeline_s3_bucket_arn.value}/*"
+            ]
+        },
+        {
+            "Sid": "AllowCodebuildReportGroup",
+            "Effect": "Allow",
+            "Action": [
+                "codebuild:CreateReportGroup",
+                "codebuild:CreateReport",
+                "codebuild:UpdateReport",
+                "codebuild:BatchPutCodeCoverages",
+                "codebuild:BatchPutTestCases"
+            ],
+            "Resource": [
+                "arn:aws:codebuild:us-east-1:${local.account_id}:report-group/${local.codebuild_project_name}-*"
+            ]
+        },
+        {
+            "Sid": "AllowLogs",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:PutLogEvents",
+                "logs:CreateLogStream"
+            ],
+            "Resource": [
+                "arn:aws:logs:us-east-1:${local.account_id}:log-group:/aws/codebuild/${local.codebuild_project_name}",
+                "arn:aws:logs:us-east-1:${local.account_id}:log-group:/aws/codebuild/${local.codebuild_project_name}:*"
+            ]
+        },
+        {
+            "Sid": "AllowSSMDescribeParameters",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeParameters"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowSSMGetParametersDocker",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameters"
+            ],
+            "Resource": [
+                "arn:aws:ssm:*:*:parameter/docker/*",
+                "arn:aws:ssm:*:*:parameter/${local.output_prefix}/codebuild/*"
+            ]
+        }
+    ]
 }
 EOF
 }
 
-resource "aws_iam_policy" "e2e_test_codebuild" {
-  name = "${var.name}-codebuild-policy"
-
-  policy = templatefile("${path.module}/codebuild_policy.tpl", {
-    account_id                   = local.account_id
-    code_pipeline_s3_bucket_name = var.code_pipeline_s3_bucket_name
-    ecr_arns                     = jsonencode(aws_ecr_repository.this.*.arn)
-    codebuild_project_name       = local.e2e_project_name
-    name                         = var.name
-  })
+resource "aws_iam_role_policy_attachment" "codebuild" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = aws_iam_policy.codebuild.arn
 }
 
-resource "aws_iam_role_policy_attachment" "e2e_test_codebuild" {
-  role       = aws_iam_role.e2e_test_codebuild.name
-  policy_arn = aws_iam_policy.e2e_test_codebuild.arn
-}
--- terraform/modules/main/main.tf
+++ terraform/modules/main/main.tf
@@ -3,11 +3,15 @@
 # A sample ECS Standalone Task meant to be run as an ad-hoc job
 #------------------------------------------------------------------------------
 
+data "aws_caller_identity" "current" {}
+
 locals {
   project_name  = "ecs-standalone-task"
   base_prefix   = [var.namespace, var.env, local.project_name]
   id_prefix     = join("-", local.base_prefix)
   output_prefix = join("/", local.base_prefix)
+
+  account_id = data.aws_caller_identity.current.account_id
 
   tags = merge(var.tags, {
     Terraform   = true
