1eeb533c96f7593be1587901c7357875ae5bdf78
ikenley <ikenley6@gmail.com>
2023-01-01 09:30:29 -0500

    cicd

--- terraform/modules/main/data.tf
+++ terraform/modules/main/data.tf
@@ -10,6 +10,10 @@ data "aws_ssm_parameter" "vpc_id" {
   name = "${local.core_prefix}/vpc_id"
 }
 
+data "aws_ssm_parameter" "code_pipeline_s3_bucket_name" {
+  name = "${local.core_prefix}/code_pipeline_s3_bucket_name"
+}
+
 data "aws_ssm_parameter" "data_lake_s3_bucket_name" {
   name = "${local.core_prefix}/data_lake_s3_bucket_name"
 }
